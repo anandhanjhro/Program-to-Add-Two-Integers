a172f1614db0147ff77b1de03b16489b9bd674b6
me <me@DESKTOP-4RS4N30>
2023-07-26 21:42:01 +0500

    Add project files.

---
+++ Program to Add Two Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program_to_Add_Two_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numb, numb2, sum;
+            Console.WriteLine("Calculate the sum of two numbers");
+            Console.Write("Input number1 ");
+            numb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input number2 ");
+            numb2 = Convert.ToInt32(Console.ReadLine());
+            sum = numb + numb2;
+            Console.Write("Result " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
